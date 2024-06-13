f247a375d8f17c726e140d83cafccc80abd6b655
Xavier Logerais <xavier.logerais-prestataire@ca-gip.fr>
2024-06-13 10:31:44 +0200

    feat: move bash config to config.d

--- config.bash
+++ config.bash
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-# Configuration BASH
-
-if [[ ! -d ~/.bash ]]
-then
-    git clone --recursive https://github.com/xlogerais/config-bash.git ~/.bash
-    bash ~/.bash/create-links.bash
-fi
-
 for file in config.d/*.bash
 do
   bash $file
---
+++ config.d/bash.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Configuration BASH
+
+if [[ ! -d ~/.bash ]]
+then
+    git clone --recursive https://github.com/xlogerais/config-bash.git ~/.bash
+    bash ~/.bash/create-links.bash
+fi
