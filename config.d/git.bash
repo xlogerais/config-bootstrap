16a3d0199305f78a9974291823237e82a1b381b5
Xavier Logerais <xavier.logerais-prestataire@ca-gip.fr>
2024-06-13 10:32:07 +0200

    feat: improve git config with global gitignore

--- config.d/git.bash
+++ config.d/git.bash
@@ -6,3 +6,13 @@ git config --global user.name "Xavier Logerais"
 git config --global user.email "xavier@logerais.com"
 git config --global rerere.enabled 1
 git config --global merge.conflictstyle diff3
+
+cat <<EOF >~/.gitignore
+# asdf
+.tool-versions
+# direnv
+.envrc
+.direnv
+EOF
+
+git config --global core.excludesfile ~/.gitignore
