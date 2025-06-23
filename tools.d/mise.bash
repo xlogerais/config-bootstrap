223555c852b1b3d41183d57ade88f12a44f82eeb
Xavier Logerais <xavier@logerais.com>
2025-06-24 01:08:12 +0200

    feat: add config for chezmoi

--- tools.d/mise.bash
+++ tools.d/mise.bash
@@ -15,15 +15,7 @@ test -x ~/.local/bin/mise && eval "$(~/.local/bin/mise activate bash)"
 # Configure mise
 #
 mise settings set experimental true
-(mise where usage &>/dev/null) || mise use -g usage # for completion
-(mise where &>/dev/null) || mise use -g usage       # for completion
 
 # Install some essential tools
-if (command -v mise &>/dev/null); then
-	# Install some plugins
-	(mise where usage &>/dev/null) || mise use -g usage # for completion
-
-	(mise where chezmoi &>/dev/null) || mise use -g chezmoi # to manage dotfiles
-
-	# mise use -g cargo:ubi # to install binaries from Github release
-fi
+(mise where usage &>/dev/null) || mise use -g usage     # for completion
+(mise where chezmoi &>/dev/null) || mise use -g chezmoi # to manage dotfiles
