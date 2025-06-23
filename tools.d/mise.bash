3aeb0650c476da242de95cd57285acbdabf62bd9
Xavier Logerais <xavier@logerais.com>
2025-06-24 00:53:41 +0200

    feat: Install chezmoi with mise

--- tools.d/mise.bash
+++ tools.d/mise.bash
@@ -8,13 +8,22 @@ if (! command -v mise &>/dev/null); then
 	curl https://mise.run | sh
 fi
 
+# Activate mise
+
+test -x ~/.local/bin/mise && eval "$(~/.local/bin/mise activate bash)"
+
+# Configure mise
+#
+mise settings set experimental true
+(mise where usage &>/dev/null) || mise use -g usage # for completion
+(mise where &>/dev/null) || mise use -g usage       # for completion
+
+# Install some essential tools
 if (command -v mise &>/dev/null); then
-	# Activate
-	eval "$(mise activate bash)"
-	mise settings set experimental true
 	# Install some plugins
 	(mise where usage &>/dev/null) || mise use -g usage # for completion
-	# mise use -g cargo:ubi # to install binaries from Github release
 
 	(mise where chezmoi &>/dev/null) || mise use -g chezmoi # to manage dotfiles
+
+	# mise use -g cargo:ubi # to install binaries from Github release
 fi
