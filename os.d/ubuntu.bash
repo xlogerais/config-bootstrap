0e2e334e11eff64a2f7aa17e48001f869cb1f0c3
Xavier Logerais <xavier@logerais.com>
2025-10-18 16:55:40 +0000

    feat(os): Add bash-completion package

--- os.d/ubuntu.bash
+++ os.d/ubuntu.bash
@@ -6,7 +6,8 @@ if (grep -q Ubuntu <(lsb_release --id --short)); then
 
     sudo apt install -y coreutils grep less tar zip unzip tree
     sudo apt install -y man manpages-fr manpages-fr-extra
-    sudo apt install -y bash zsh fish
+    sudo apt install -y bash bash-completion
+    sudo apt install -y zsh fish
     sudo apt install -y screen tmux
     sudo apt install -y git #tig
     sudo apt install -y vim
