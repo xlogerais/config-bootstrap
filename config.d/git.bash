09f940169f5d7853f74d08489bee5e6365944614
Xavier Logerais <xavier@logerais.com>
2025-10-29 22:15:44 +0100

    feat(config): Disable git configuration
    This is now managed with [chezmoi](https://www.chezmoi.io/) in the
    repository
    [config-chezmoi](https://gitea.logerais.com/xavier/config-chezmoi).

--- config.d/git.bash
+++ config.d/git.bash
@@ -2,28 +2,30 @@
 
 # Configuration GIT
 
-git config --global user.name "Xavier Logerais"
-git config --global user.email "xavier@logerais.com"
+# Now managed by chezmoi
 
-git config --global credential.helper cache
-
-git config --global merge.conflictstyle diff3
-git config --global rerere.enabled 1
-
-cat <<EOF >~/.gitignore
-### asdf ###
-.tool-versions
-### mise ###
-.mise.toml
-### direnv ###
-.direnv
-.envrc
-### python ###
-.venv
-EOF
-
-cat <<EOF >~/.gitattributes
-EOF
-
-git config --global core.excludesfile ~/.gitignore
-git config --global core.attributesfile ~/.gitattributes
+# git config --global user.name "Xavier Logerais"
+# git config --global user.email "xavier@logerais.com"
+# 
+# git config --global credential.helper cache
+# 
+# git config --global merge.conflictstyle diff3
+# git config --global rerere.enabled 1
+# 
+# cat <<EOF >~/.gitignore
+# ### asdf ###
+# .tool-versions
+# ### mise ###
+# .mise.toml
+# ### direnv ###
+# .direnv
+# .envrc
+# ### python ###
+# .venv
+# EOF
+# 
+# cat <<EOF >~/.gitattributes
+# EOF
+# 
+# git config --global core.excludesfile ~/.gitignore
+# git config --global core.attributesfile ~/.gitattributes
